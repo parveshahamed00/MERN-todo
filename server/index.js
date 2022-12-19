dc340c314ecb76dbd72a630372c11f73484749ba
Parvesh <parveshahamed00@gmail.com>
2022-12-19 10:57:20 +0530

    adding mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const mongoose=require("mongoose")
+require("dotenv").config(); // for environment variables
+
 const app = express()
 const port = 4000
-
-app.get('/', (req, res) => res.send('Hello World!'))
+// mongoDB connection
+mongoose.set("strictQuery", false);
+const uri = process.env.mongoDB;
+async function connect() {
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    mongoose.set('strictQuery', false)
+    console.log("Scuessfully Connected to database");
+  } catch (error) {
+    console.log(error);
+  }
+}
+connect();
+app.use("/home",require("./routes/home"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
