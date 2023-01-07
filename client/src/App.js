70e70765a791cffa20f31b973ffac6e6620b7161
Parvesh <parveshahamed00@gmail.com>
2023-01-07 20:26:02 +0530

    adding sort functionality and date

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TodoLists from "./components/TodoLists";
 import CreateTodo from "./components/CreateTodo";
+import { BsArrowDownUp } from "react-icons/bs";
+
 function App() {
   const [todoList, setTodoList] = useState([]);
   const [popup, setPopup] = useState(false);
@@ -11,7 +13,7 @@ function App() {
     axios
       .get("http://localhost:4000/home/")
       .then((e) => {
-        // console.log(e.data);
+        console.log(e.data);
         setTodoList(e.data);
       })
       .catch((e) => {
@@ -21,19 +23,19 @@ function App() {
   function closePopup() {
     setPopup(false);
   }
-  function newTodo(newtodo){
-      // console.log("new todo");
-      // console.log(newtodo);
-setTodoList([...todoList,newtodo])
+  function newTodo(newtodo) {
+    setTodoList([...todoList, newtodo]);
   }
-  function openPopup(){
-        setPopup(true);
-}
-function removeTodo(title){
- 
-  let list=todoList.filter((e)=> e.title!==title) 
-  setTodoList(list)
-}
+  function openPopup() {
+    setPopup(true);
+  }
+  function removeTodo(title) {
+    let list = todoList.filter((e) => e.title !== title);
+    setTodoList(list);
+  }
+  function sorttodo() {
+    setTodoList([...todoList].reverse());
+  }
   return (
     <div className="App">
       <Navbar></Navbar>
@@ -48,6 +50,9 @@ function removeTodo(title){
             Welcome, Parvesh Ahamed<span className="wave">👋</span>
           </h4>
           <div className="todoList">
+            <div className="sort-icon">
+              <BsArrowDownUp onClick={sorttodo}></BsArrowDownUp>
+            </div>
             <div className="lists">
               <TodoLists list={todoList} removeTodo={removeTodo}></TodoLists>
             </div>
--- client/src/components/TodoLists.js
+++ client/src/components/TodoLists.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React,{useState} from "react";
 import "./TodoLists.css";
 import { BsCheck2Square, BsTrash } from "react-icons/bs";
 import axios from "axios";
 
 
 function TodoLists(props) {
+  const[isTaskCompleted,setisTaskCompleted]=useState(true)
   function removeTodo(e){
     console.log(e._id);
     axios.delete(`http://localhost:4000/home/${e._id}`).then((x)=>{
@@ -14,12 +15,16 @@ function TodoLists(props) {
       console.log(err);
     })
   }
+  function isTaskcompleted(){
+    console.log("clicked");
+  }
   const list = props.list.map((e, index) => {
     const tasklist = e.tasks.map((e, index) => {
       return (
         <p key={index} className="task">
           <BsCheck2Square
-            className={e.iscomplete ? "iscomplete" : "notcomplete"}
+            onClick={isTaskCompleted}
+            className={isTaskCompleted ? "iscomplete" : "notcomplete"}
           ></BsCheck2Square>{" "}
           {e.task}
         </p>
@@ -27,8 +32,9 @@ function TodoLists(props) {
     });
     return (
       <div key={index} className="todoLists">
+        <p className="generated-date">{e.date}</p>
         <h2 className="title">
-          {e.title}
+          {e.title} 
          <span className="dustbin"><BsTrash onClick={()=>{removeTodo(e);}}></BsTrash></span>
         </h2>
         <div >
